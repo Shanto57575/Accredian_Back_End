a47dbd305afa73069fa9ea79ebaa5ae70aeeec6a
Shanto57575 <shanto57575@gmail.com>
2024-07-06 00:45:54 +0600

    changed

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Accredian API IS WORKING FINE!" })
+})
+
 app.post('/api/referral', async (req, res) => {
     const { yourName, yourEmail, friendName, friendEmail } = req.body;
 
